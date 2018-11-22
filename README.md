efe5351ae950cd7dd59162c0a5406c204b0f2031
wzz <1372633952@qq.com>
2018-11-22 20:08:59 +0800

    update: share

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# mini-magicui
-支付宝组件
+# magic-alipay
+
+## 安装
+
+```bash
+$ npm install magic-alipay --save
+```
+
+## 使用
+
+在页面json中文件中进行注册，如share组件的注册如下所示：
+
+```json
+{
+  "usingComponents": {
+    "share": "mini-magicui/es/share/index",
+  }
+}
+```
+
+在axml文件中进行调用：
+```html
+
+```
---
+++ src/share/index.js
@@ -0,0 +1,33 @@
+Component({
+  data: {
+    appId: '2018110762077090'
+  },
+  props: {
+   name: '',
+   des: '',
+   icon: '',
+   pageUrl: '',
+   shareList: []
+  },
+  methods: {
+    share() {
+      my.navigateToMiniProgram({
+        appId: this.data.appId,
+        envVersion: 'develop',
+        extraData: {
+          name: this.props.name,
+          des: this.props.des,
+          icon: this.props.icon,
+          pageUrl: this.props.pageUrl,
+          shareList: this.props.shareList
+        },
+        success: (res) => {
+          // console.log(JSON.stringfy(res))
+        },
+        fail: (res) => {
+          // console.log(JSON.stringfy(res))
+        }
+      });
+    }
+  }
+});
